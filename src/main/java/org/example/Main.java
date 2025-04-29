dccc89796dd90d4d6d12a5defb8ed46b4c633cf5
Kourosh Maissamyazad <2440557@edu.vaniercollege.qc.ca>
2025-04-29 15:35:51 -0400

    Changed bubbleSort to sort in chosen order in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,14 +15,22 @@ public class Main {
         insertionSort(nums);
         System.out.println(Arrays.toString(nums));
 
+        bubbleSort(nums, false);
+        System.out.println(Arrays.toString(nums));
+
 
     }
 
-
     public static void bubbleSort(int[] nums) {
+        bubbleSort(nums, true);
+    }
+
+    public static void bubbleSort(int[] nums, boolean ascending) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length - 1 - i; j++) {
-                if (nums[j] > nums[j + 1]) {
+                boolean condition = ascending ? nums[j] > nums[j + 1] : nums[j] < nums[j + 1];
+                if (condition) {
+                    // swap
                     int temp = nums[j];
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
@@ -30,7 +38,6 @@ public class Main {
             }
         }
     }
-
 
     public static void selectionSort(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
